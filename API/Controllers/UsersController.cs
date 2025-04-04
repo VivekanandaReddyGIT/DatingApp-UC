15c474400ab76296f705c94556d2a95698b439f2
Vivekananda Reddy <vivekanandareddy208@gmail.com>
2025-04-05 00:02:25 +0530

    End of Section 7

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,25 +1,29 @@
 using API.Controllers;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API;
-
-public class UsersController(DataContext context): BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
-  [AllowAnonymous]
   [HttpGet]
-  public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers(){
-    var users = await context.Users.ToListAsync();
-    return Ok(users);
+  public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
+  {
+    var users = await userRepository.GetUsersAsync();
+    var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+    return Ok(usersToReturn);
   }
-  [Authorize]
-  [HttpGet("{id:int}")]
-  public async Task<ActionResult<IEnumerable<AppUser>>> GetUser(int id){
-    var user = await context.Users.FindAsync(id);
+  [HttpGet("{username}")]
+  public async Task<ActionResult<MemberDto>> GetUser(string username)
+  {
+    var user = await userRepository.GetUserByUsernameAsync(username);
     if (user == null) return NotFound();
-    return Ok(user);
+    return mapper.Map<MemberDto>(user);
   }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,8 +6,8 @@ public class AppUser
 {
   public int Id { get; set; }
   public required string UserName { get; set; }
-  public required byte[] PasswordHash { get; set; } = [];
-  public required byte[] PasswordSalt { get; set; } = [];
+  public byte[] PasswordHash { get; set; } = [];
+  public byte[] PasswordSalt { get; set; } = [];
   public DateOnly DateOfBrith { get; set; }
   public required string KnownAs { get; set; }
   public DateTime Created { get; set; } = DateTime.UtcNow;
